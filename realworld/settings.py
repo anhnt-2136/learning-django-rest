1451f2a8b01559444a8928e7af598307881b71c0
Nguyen Tuan Anh M (anhnt-2136) <nguyen.tuan.anhm@sun-asterisk.com>
2025-07-31 18:24:42 +0700

    feat(throttling): add default throttle classes and rates for API requests

--- realworld/settings.py
+++ realworld/settings.py
@@ -50,6 +50,11 @@ REST_FRAMEWORK = {
         "rest_framework.pagination.PageNumberPagination"
     ),
     "PAGE_SIZE": 20,
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "100/day", "user": "1000/day"},
 }
 
 # Application definition
